5700a8c244e583a07ea3ee19b50e4e9d7747d53f
王十二 <tongqianwen@foxmail.com>
2020-09-20 18:42:53 +0800

    完成setState()功能

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 import {createElement, Component, render} from "./toy-react"
 
 class MyComponent extends Component{
+    constructor(){
+        super();
+        this.state = {
+            a: 1,
+            b:2
+        }
+    }
     render(){
         return <div>
             <h1>my Component</h1>
-            {this.children}
+            <button onclick={()=>{ this.setState({a:this.state.a++}) }}>add</button>
+    <span>{this.state.a.toString()}</span>
+    <span>{this.state.b.toString()}</span>
         </div>
     }
 }
